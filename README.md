0efff8fc940a1d942cf456b46671456b3e670cea
qwtsc <c.shen@sjtu.edu.cn>
2020-12-13 15:03:00 +0800

    wait to test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Demo for longerpump control
+
+As is a very simple demo repo, code can explain everything. The only thing that should be noted is that we have to sleep aroung 1 sec to wait for the reply from pump after sending every command.
+
--- main.py
+++ main.py
@@ -17,34 +17,35 @@ def innerXOR(b):
     return bytes([first])
 
 
-def send_command(s, b):
-    flag = b"\xE9"
-    addr = b"\x02"
-    lenb = len(b).to_bytes(1, byteorder="little")
-    first = bxor(innerXOR(b), bxor(addr, lenb))
-    command = flag + addr + lenb + b + first
-    print(command)
-    s.flushOutput()
-    s.write(command)
-    s.flushOutput()
-    time.sleep(0.8)
-    print(s.read_all())
-    return
-
-
 class Syringe():
-    def __init__(self, port):
+    def __init__(self, port, addr=b'\x01'):
+        # default address is 1, default baudrate is 9600
         self.port = port
+        self.addr = addr
         self.serial = serial.Serial(
             port=self.port, baudrate=9600, bytesize=8, stopbits=1, parity=serial.PARITY_EVEN)
 
+    def send_command(self, b):
+        flag = b"\xE9"
+        addr = self.addr
+        lenb = len(b).to_bytes(1, byteorder="little")
+        first = bxor(innerXOR(b), bxor(addr, lenb))
+        command = flag + addr + lenb + b + first
+        print(command)
+        self.serial.flushOutput()
+        self.serial.write(command)
+        self.serial.flushOutput()
+        time.sleep(0.8)
+        print(s.read_all())
+
     def run(self):
-        send_command(self.serial, b'CWX'+b'\x01')
+        send_command(b'CWX'+b'\x01')
 
     def stop(self):
-        send_command(self.serial, b'CWX'+b'\x02')
+        send_command(b'CWX'+b'\x02')
 
     def close(self):
+        self.stop()
         self.serial.close()
 
     def reconnect(self):
@@ -52,11 +53,12 @@ class Syringe():
             port=self.port, baudrate=9600, bytesize=8, stopbits=1, parity=serial.PARITY_EVEN)
 
     def setFlowRate(self, volume=2000, flowrate=100):
-        # default volume to purge is 20 ml, the unit of flowrate is 0.01 ml/min
-        send_command(self.serial, b'CWT'+b'\x01' +
-                     volume.to_bytes(2, byteorder='little')+b'\x05'+flowrate.to_bytes(2, byteorder='little')+b'\x0C')
+        # default volume to deliver is 20 ml, the unit of volume is 0.01ml. the unit of flowrate is 0.01 ml/min
+        send_command(b'CWT'+b'\x01' + volume.to_bytes(2, byteorder='little') +
+                     b'\x05'+flowrate.to_bytes(2, byteorder='little')+b'\x0C')
 
     def schedule(self, t, a5, a4, a3, a2, a1, a0):
+        # t in sec, res in ml/min
         res = a5 * math.pow(t, 5) + a4 * math.pow(t, 4) + a3 * \
             math.pow(t, 3) + a2 * math.pow(t, 2) + a1 * t + a0
         if res > 9999:
@@ -64,11 +66,12 @@ class Syringe():
             return -1
         return int(res*100)
 
-    def runWithSchedule(self, a5, a4, a3, a2, a1, a0):
+    def runWithSchedule(self, a5, a4, a3, a2, a1, a0, end_time):
+        # end_time in sec
         start = time.time()
         onGoing = time.time() - start
         self.run()
-        while onGoing <= 120:
+        while onGoing <= end_time:
             rate = self.schedule(onGoing, a5, a4, a3, a2, a1, a0)
             if rate > 0:
                 print(rate)
@@ -81,16 +84,9 @@ class Syringe():
 
 
 # Open serial port
-# s = serial.Serial('/dev/ttyACM0',115200)
 if __name__ == "__main__":
     sp = Syringe(port="/dev/ttyUSB0")
-    sp.runWithSchedule(0, 0, 0, 0, -1.0/15.0, 10)
-    # sp.setFlowRate(volume=6000, flowrate=1000)  # unit = 0.01ml/min
-    # time.sleep(2)
-    # sp.run()
-   # time.sleep(60)  # unit=s
-   # sp.run()
-    # time.sleep(2)
+    sp.runWithSchedule(0, 0, 0, 0, -1.0/15.0, 10, end_time=120)
    # sp.setFlowRate(flowrate=200)  # unit = 0.01ml/min
    # time.sleep(5)  # unit=s
    # sp.setFlowRate(flowrate=300)  # unit = 0.01ml/min
@@ -101,5 +97,3 @@ if __name__ == "__main__":
    # time.sleep(3)  # unit=s
     # sp.stop()
     sp.close()
-
-# Wake up
